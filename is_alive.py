45aa15a514ffae96e91e07aa669d28de8e5991a3
Yellowsquared <yellowsquared.llc@gmail.com>
2025-06-16 21:15:29 -0700

    Init

---
+++ is_alive.py
@@ -0,0 +1,20 @@
+from prefect import flow
+from pathlib import Path
+import httpx
+
+
+@flow
+def main() -> list[str]:
+    response = httpx.get("https://api.yellowsquared.us/health")
+    response.raise_for_status()
+    assert response.json()["status"] == "healthy"
+
+
+if __name__ == "__main__":
+    main.from_source(
+        source=str(Path(__file__).parent),
+        entrypoint="is_alive.py:main",
+    ).deploy(
+        name="is_alive",
+        work_pool_name="api_status"
+    )
